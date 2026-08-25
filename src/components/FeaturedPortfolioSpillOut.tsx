365df0b681c00b7192439ebc581a374e92f01bc8
clickmetostart <clickme.tostart@gmail.com>
2026-08-25 00:28:16 -0500

    reduce padding between title and pinned image to 8px

--- src/components/FeaturedPortfolioSpillOut.tsx
+++ src/components/FeaturedPortfolioSpillOut.tsx
@@ -40,7 +40,7 @@ export default function FeaturedPortfolioSpillOut() {
 
         {/* Overlay Title that fades out as you scroll */}
         <motion.div 
-          className="relative z-30 pointer-events-none flex flex-col items-center mb-[30px]"
+          className="relative z-30 pointer-events-none flex flex-col items-center mb-2"
           style={{
             opacity: useTransform(scrollYProgress, [0, 0.1, 1], [1, 0, 0]),
             y: useTransform(scrollYProgress, [0, 0.1, 1], [0, -50, -50])
